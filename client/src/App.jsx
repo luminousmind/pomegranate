60078793621d242a0b6a0fd356655dcac8674ee8
luminousmind <193550240+luminousmind@users.noreply.github.com>
2025-01-31 18:42:24 +1100

    feat: add home, signup and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <>
       <Navbar />
+      <main className="flex-center">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </main>
     </>
   );
 }
