30122316bea892aa5f677e327ac9b03f43dcd633
luminousmind <193550240+luminousmind@users.noreply.github.com>
2025-01-31 21:03:35 +1100

    feat: update navbar and design basic signup page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,16 +3,24 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <header>
-      <header className="flex-center">
-        <nav className="flex-between">
-          <Link to="/">
-            <h3>superlogo</h3>
+      <header className="flex-center bg-[#181818] h-14">
+        <nav className="flex-between w-full max-w-6xl">
+          <Link to="/" tabIndex="-1">
+            <h3 className="font-bold text-lg hocus:text-white" tabIndex="0">
+              superlogo
+            </h3>
           </Link>
-          <div className="flex-center auth-div">
-            <Link to="/login" className="btn">
+          <div className="flex-center gap-x-2">
+            <Link
+              to="/login"
+              className="cursor-pointer px-3 py-1.5 rounded-xl bg-[#242424] border-[#2e2e2e] border-1 hocus:brightness-125 duration-300"
+            >
               Login
             </Link>
-            <Link to="/signup" className="btn cta">
+            <Link
+              to="/signup"
+              className="cursor-pointer px-3 py-1.5 rounded-xl bg-[#17613b] border-[#298255] border-1 hocus:brightness-125 duration-300"
+            >
               Sign Up
             </Link>
           </div>
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 function SignupPage() {
-  return <h1>Signup Page</h1>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>Welcome to App</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="cursor-pointer px-3 py-1.5 rounded-xl bg-[#17613b] border-[#298255] border-1 hocus:brightness-125 duration-300"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SignupPage;
